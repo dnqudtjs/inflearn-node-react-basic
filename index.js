00bc39933bd1920a874fc0c68d8811e5b2cf9e11
woo <dnqudtjs@gmail.com>
2020-07-04 22:47:49 +0900

    #11 로그인기능 #12 토큰 생성 with jsonwebtoken

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = 3000;
 const { User } = require("./models/User");
 const bodyParser = require("body-parser");
 
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 // application/x-www.form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -31,6 +33,32 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일 유저 없습니다.",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "비밀번호 틀림" });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
